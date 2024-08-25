853b9743574b7e4ffcaf1dba7c56793f750ee8e6
Sahilgitlover <nagpalsahil2020@gmail.com>
2024-08-25 12:39:59 +0530

    Added is above

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const name = "sahil";
+const name1 = "sahil";
 
 function a() {
     console.log("My name is" + name);
-    
 }
 a(); 
+
+let test1 = "abc"
+
+function isAvoe(){
+    return age >= 18;
+}
--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function a( A,  B){
     return A + B;
 }
 a(5,3);
+
+let sahil = "sja";
+
+let test = "abc"
